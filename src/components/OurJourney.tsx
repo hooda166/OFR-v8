a6aeeadf0823d64d77e217edb59fbb9238247a23
Shreshth Hooda <hooda@MacBook-Pro.local>
2025-11-09 18:16:38 +0530

    Initial commit

--- src/components/OurJourney.tsx
+++ src/components/OurJourney.tsx
@@ -19,7 +19,7 @@ const OurJourney = () => {
             />
             <img
               src="/assets/Strategic expansion-2-pexels.jpg"
-              alt="Infrastructure revolution 2"
+              alt="Strategic Expansion 2"
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
           </div>
@@ -55,12 +55,12 @@ const OurJourney = () => {
           </p>
           <div className="grid grid-cols-2 gap-4 mb-6">
             <img
-              src="/assets/Infrastructure revolution-1 pexels.jpg"
+              src="/assets/Infrastructure revolution-1-pexels.jpg"
               alt="Infrastructure revolution 1"
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <img
-              src="/assets/Infrastructure revolution-2 pexels.jpg"
+              src="/assets/Infrastructure revolution-2-pexels.jpg"
               alt="Infrastructure revolution 2"
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
